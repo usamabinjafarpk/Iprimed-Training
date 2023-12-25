8a7491f172b4a411ba8d13b9f2e184a8bedbb1a1
USAMA BIN JAFAR PK <67426021+usamabinjafarpk@users.noreply.github.com>
2023-12-25 13:10:56 +0530

    Second Commit

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Assignment2
+namespace Day2Assignment
 {
     internal class Program
     {
@@ -8,13 +8,45 @@
             //a)   	Print the character at the 12th index.
             Console.WriteLine(str[12]);
             //b)	Check whether the String contains the word “is”.
+            if (str.Contains("is"))
+            {
+                Console.WriteLine("Found");
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+
             //c)	Add the string “and killed it” to the existing string.
             string str1 = "and killed it";
-            Console.WriteLine(str+" "+str1);
+            Console.WriteLine(str + " " + str1);
             //d)	Check whether the String ends with the word “dogs”.
+
+            int dind=str.LastIndexOf("d");
+            
+            if (dind!=-1)
+            {
+                string last = str.Substring(dind);
+                if (last == "dog")
+                {
+                    Console.WriteLine("Dog is found");
+                }
+                else
+                {
+                    Console.WriteLine("Dog is not found");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Dog is not found");
+            }
+
+
+
             //e)	Check whether the String is equal to “The quick brown Fox jumps over the lazy Dog”.
             string str2 = "The quick brown Fox jumps over the lazy Dog";
-            if(str.Equals(str2))
+            if (str.Equals(str2))
             {
                 Console.WriteLine("String is equal");
             }
@@ -24,16 +56,57 @@
             }
             //f)Check whether the String is equal to “THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG”.    
             string str3 = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
-            if (str.Equals(str3))
+            if (str.Equals(str3, StringComparison.OrdinalIgnoreCase))
             {
-                Console.WriteLine("String is equal");
+                Console.WriteLine("String is Equal");
             }
             else
             {
-                Console.WriteLine("String is not equal");
+                Console.WriteLine("String is not Equal");
             }
             //g)	Find the length of the String.
             Console.WriteLine(str.Length);
+            //h)  	Check whether the String matches to “The quick brown Fox jumps over the lazy Dog”.
+            string str4 = "The quick brown Fox jumps over the lazy Dog";
+            if (str.Equals(str4))
+            {
+                Console.WriteLine("String is Equal");
+            }
+            else
+            {
+                Console.WriteLine("String is not Equal");
+            }
+
+            //i)        Replace the word “The” with the word “A”.
+            Console.WriteLine(str.Replace("The", "A"));
+
+            //j)        Split the above string into two such that two animal names do not come together.
+
+            //k)   	Print the animal names alone separately from the above string.
+            Console.WriteLine(str[16]);
+            //l)        Print the above string in completely lower case.
+            Console.WriteLine(str.ToLower());
+
+            //m)	Print the above string in completely upper case.
+            Console.WriteLine(str.ToUpper());
+
+            //n)  	Find the index position of the character “a”.
+            Console.WriteLine(str.IndexOf("a"));
+
+            //o)  	Find the last index position of the character “e”.
+            Console.WriteLine(str.LastIndexOf("e"));
+
+            //p)  	Prompt the user to enter the home directory of Tomcat server. To the path that user enters, add another path to  WebApps/MyApps/Images  directory and display it in the console. Use verbatim string literals.
+            Console.WriteLine("Enter the path:");
+            string path = Console.ReadLine();
+            Console.WriteLine(path + @"/WebApps/MyApps/Images");
+
+            /*q)    Write a C# code that has a constant string that represents poem lines
+                    I WANDER'D lonely as a cloud
+                    That floats on high o'er vales and hills, */
+            const string Poem = "I WANDER'D lonely as a cloud\nThat floats on high o'er vales and hills";
+            Console.WriteLine(Poem);
+
         }
-    }
+}
 }
