5db9573047f03f2264980f5f5c32dc9b0e12b6e6
USAMA BIN JAFAR PK <67426021+usamabinjafarpk@users.noreply.github.com>
2024-01-03 15:50:11 +0530

    Add files via upload

---
+++ APIAssignment1/Controllers/BookController.cs
@@ -0,0 +1,140 @@
+﻿using APIAssignment1.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using APIAssignment1.Models;
+
+namespace APIAssignment1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository bookRepository;
+        public BookController()
+        {
+            bookRepository = new BookRepository();
+        }
+        [HttpPost("AddBook")]
+        public IActionResult Add(Book book)
+        {
+            try
+            {
+                bookRepository.AddBook(book);
+                return StatusCode(200, book);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+        [HttpGet("GetAllBook")]
+        public IActionResult GetAllBooks()
+        {
+            List<Book> books = bookRepository.GetAllBooks();
+            try
+            {
+                return StatusCode(200, books);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+        [HttpGet("GetBookByAuthor/{author}")]
+        public IActionResult GetBookByAuthor(string author)
+        {
+            try
+            {
+                return StatusCode(200, bookRepository.GetBookByAuthor(author));
+            }
+            catch(Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+        [HttpGet("GetBookByLanguage/{lang}")]
+
+        public IActionResult GetBookByLang(string lang)
+        {
+            try
+            {
+                return StatusCode(200, bookRepository.GetBookByLanguage(lang));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+        [HttpGet("GetBookByYear/{year}")]
+        public IActionResult GetBookByYear(int year)
+        {
+            try
+            {
+                return StatusCode(200, bookRepository.GetBookByYear(year));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+        [HttpPut("EditBook")]
+        public IActionResult EditBook(Book book)
+        {
+            try
+            {
+                bookRepository.EditBook(book);
+                return StatusCode(200, book);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+        [HttpDelete("DeleteBook/{bookId}")]
+        public IActionResult DeleteBook(int bookId)
+        {
+            try
+            {
+                bookRepository.DeleteBook(bookId);
+                return StatusCode(200, "Book deleted successfully");
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+        [HttpGet("GetBookById/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            Book book = bookRepository.GetBookById(id);
+            if(book!=null)
+            {
+                return StatusCode(200, book);
+            }
+            else
+            {
+                return StatusCode(405);
+            }
+        }
+        [HttpGet("GetBookByName/{name}")]
+        public IActionResult GetBookByName(string name)
+        {
+            Book book = bookRepository.GetBookByName(name);
+            if (book != null)
+            {
+                return StatusCode(200, book);
+            }
+            else
+            {
+                return StatusCode(405);
+            }
+        }
+    }
+}
---
+++ APIAssignment1/Repositories/BookRepository.cs
@@ -0,0 +1,154 @@
+﻿using APIAssignment1.Models;
+
+namespace APIAssignment1.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        public static List<Book> books = new List<Book>()
+        {
+            new Book(){BookId=121,BookName="abc",Author="qwe",Lang="English",ReleaseYear=2020,Publisher="qaz",Price=999}
+        };
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            try
+            {
+                foreach (var book in books)
+                {
+                    if (book.BookId == bookId)
+                    {
+                        books.Remove(book);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void EditBook(Book book)
+        {
+            try
+            {
+                foreach (var item in books)
+                {
+                    if (item.BookId == book.BookId)
+                    {
+                        /*book.Author = item.BookName;
+                        book.BookName = item.BookName;*/
+
+                        item.Author=book.Author;
+                        item.BookName=item.BookName;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return books;
+        }
+
+        public List<Book> GetBookByAuthor(string author)
+        {
+            List<Book> book = new List<Book>();
+            try
+            {
+                foreach (var item in books)
+                {
+                    if (item.Author == author)
+                    {
+                        book.Add(item);
+                    }
+                }
+                return book;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        
+
+        public Book GetBookById(int id)
+        {
+            foreach(var book in books)
+            {
+                if(book.BookId==id)
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+
+        public List<Book> GetBookByLanguage(string lang)
+        {
+            List <Book> book = new List<Book>();
+            try
+            {
+                foreach (var item in books)
+                {
+                    if (item.Lang == lang)
+                    {
+                        book.Add(item);
+                    }
+                }
+                return book;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Book GetBookByName(string name)
+        {
+            foreach (var book in books)
+            {
+                if (book.BookName == name)
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+
+        public List<Book> GetBookByYear(int year)
+        {
+            List<Book> book = new List<Book>();
+            try
+            {
+                foreach (var item in books)
+                {
+                    if (item.ReleaseYear == year)
+                    {
+                        book.Add(item);
+                    }
+                }
+                return book;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+          
+
+        }
+    }
+}
